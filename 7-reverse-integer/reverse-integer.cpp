d17f9f4f8383e141b9d685b6d41e2b0e8bae3abd
YASH DIVYA <121162537+yashdivya5@users.noreply.github.com>
2025-04-18 17:58:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (54.46%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -4,8 +4,7 @@ public:
         int num=0;
         while(x!=0){
             int rem=x%10;
-            if (num > INT_MAX / 10 || (num == INT_MAX / 10 && rem > 7)) return 0;
-            if (num < INT_MIN / 10 || (num == INT_MIN / 10 && rem < -8)) return 0;
+            if(num>INT_MAX/10 || num<INT_MIN/10) return 0;
             num=num*10+rem;
             x=x/10;
         }
