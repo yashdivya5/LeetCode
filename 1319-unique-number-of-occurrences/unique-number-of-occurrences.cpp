d08b55bbf3ac809f5c399eeae250cd8c89e9c8da
YASH DIVYA <121162537+yashdivya5@users.noreply.github.com>
2025-05-03 12:21:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (95.74%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -5,7 +5,7 @@ public:
         vector<int>v;
         
         for(int i=0;i<arr.size();i++){
-            int count=1;
+            int count=0;
             while(i+1<arr.size() && arr[i]==arr[i+1]){
                 count++;
                 i++;
