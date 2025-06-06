a840b847f65901caa78113fdb6b8f6a694c8b72d
YASH DIVYA <121162537+yashdivya5@users.noreply.github.com>
2025-06-06 16:12:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (14.75%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    int BinSearch(long long n) {
-        long long s = 0;
-        long long e = n;
-        long long ans = -1;
 
-        while (s <= e) {
-            long long mid = s + (e - s) / 2;
-            long long square = (long long)mid * mid; // ✅ cast to long long
+    int BinSearch(int n){
+        long long s=0;
+        long long e=n;
+        long long mid=s+(e-s)/2;
+        long long ans=-1;
 
-            if (square == n) {
+        while(s<=e){
+            long long square = mid*mid;
+            if(square == n){
                 return mid;
             }
-            if (square > n) {
-                e = mid - 1;
-            } else {
-                ans = mid;
-                s = mid + 1;
+            if(square>n){
+                e=mid-1;
             }
+            else{
+                ans=mid;
+                s=mid+1;
+            }
+            mid=s+(e-s)/2;
         }
         return ans;
     }
-
     int mySqrt(long long x) {
         return BinSearch(x);
     }
