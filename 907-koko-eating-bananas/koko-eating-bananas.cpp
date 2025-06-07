c970aa4a0f1f9aeffdce7aa13b090acd4d47cd85
YASH DIVYA <121162537+yashdivya5@users.noreply.github.com>
2025-06-07 12:14:21 +0530

    Time: 2 ms (96.56%) | Memory: 22.7 MB (96.58%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPos(vector<int>& arr, int h, int k) {
+       long long hours=0;
+       for(int i=0; i<arr.size();i++){
+        hours+=(arr[i] + k - 1) / k;
+       }
+       if(hours<=h){
+        return true;
+       }
+       else{
+        return false;
+       }
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int s = 1;
+        int e = *max_element(piles.begin(), piles.end());;
+        int ans = e;
+        int mid = s + (e - s) / 2;
+        while (s <= e) {
+            if (isPos(piles, h, mid)) {
+                ans = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+            mid = s + (e - s) / 2;
+        }
+        return ans;
+    }
+};
