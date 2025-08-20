ec8ec8fa72f71261507abd9f3268811053427cd9
YASH DIVYA <121162537+yashdivya5@users.noreply.github.com>
2025-08-20 09:02:44 +0530

    Time: 9 ms (95.95%) | Memory: 59.9 MB (98.17%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -4,9 +4,9 @@ public:
         if(nums.size()==0) return 0;
         sort(nums.begin(),nums.end());
         int longest=1,count=1;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i] == nums[i-1]) continue;
-            if(nums[i] == nums[i-1] + 1) count++;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i] == nums[i+1]) continue;
+            if(nums[i]+1 == nums[i+1] ) count++;
             else{
                 longest=max(longest,count);
                 count=1;
